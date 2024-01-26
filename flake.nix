4a122dd1e6235cf9d190b0592fb9994d5cfefae5
Stefan Frijters <sfrijters@gmail.com>
2024-01-26 23:51:32 +0100

    Use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,86 +1,87 @@
 {
   description = "Tests for Seeed Studio XIAO ESP32C3";
   inputs = {
-    # Moving to release-23.11 causes errors:
-    #   >   = note: x86_64-unknown-linux-gnu-gcc: error: unrecognized command-line option '-flavor'
-    #   >           x86_64-unknown-linux-gnu-gcc: error: unrecognized command-line option '--as-needed'; did you mean '-mno-needed'?
-    #   >           x86_64-unknown-linux-gnu-gcc: error: unrecognized command-line option '--gc-sections'; did you mean '--data-sections'?
-    #   >
     nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay }: let
-    inherit (nixpkgs) lib;
-    systems = ["aarch64-darwin" "x86_64-linux" "aarch64-linux"];
-    systemClosure = attrs:
-      builtins.foldl' (acc: system:
-        lib.recursiveUpdate acc (attrs system)) {}
-      systems;
-  in
-    systemClosure (
-      system: let
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ] (system:
+      let
+        inherit (nixpkgs) lib;
         inherit ((builtins.fromTOML (builtins.readFile ./Cargo.toml)).package) name;
 
         nixpkgs-patched = (import nixpkgs { inherit system; }).applyPatches {
           name = "cargo-linker-fix";
           src = nixpkgs;
+          # Fix:
+          #   >   = note: x86_64-unknown-linux-gnu-gcc: error: unrecognized command-line option '-flavor'
+          #   >           x86_64-unknown-linux-gnu-gcc: error: unrecognized command-line option '--as-needed'; did you mean '-mno-needed'?
+          #   >           x86_64-unknown-linux-gnu-gcc: error: unrecognized command-line option '--gc-sections'; did you mean '--data-sections'?
+          #   >
+          # by forcing LLD as a linker
+          # Also make things more verbose.
           patches = [ ./cargo-linker-fix.patch ];
         };
 
         pkgs = import nixpkgs-patched {
           inherit system;
-          overlays = [(import rust-overlay)];
+          overlays = [
+            (import rust-overlay)
+          ];
+        };
+
+        pkgsCross = import nixpkgs-patched {
+          inherit system;
+          # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md#cross-compilation-cross-compilation ?
+          crossSystem = lib.systems.examples.riscv32-embedded // {
+            rustc.config = "riscv32imc-unknown-none-elf";
+          };
         };
 
         toolchain = (
           pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml
         );
-        rustPlatform = let
-          pkgsCross = import nixpkgs-patched {
-            inherit system;
-            # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md#cross-compilation-cross-compilation ?
-            crossSystem = lib.systems.examples.riscv32-embedded // {
-              rustc.config = "riscv32imc-unknown-none-elf";
-            };
-          };
-        in
-          pkgsCross.makeRustPlatform
-          {
-            rustc = toolchain;
-            cargo = toolchain;
-          };
-      in {
-        packages.${system}.default = pkgs.callPackage ./. {
-          inherit name rustPlatform;
+
+        rustPlatform = pkgsCross.makeRustPlatform {
+          rustc = toolchain;
+          cargo = toolchain;
         };
 
-        devShells.${system}.default = pkgs.mkShell ({
-          name = "${name}-dev";
-          buildInputs = [
-            pkgs.cargo-espflash
-            pkgs.cargo-generate
-            toolchain
-          ];
-        } // (import ./wifi-settings.nix));
+      in
+        {
+          packages.default = pkgs.callPackage ./. {
+            inherit name rustPlatform;
+          };
 
-        apps.${system} = {
-          default = let
+          devShells.default = pkgs.mkShell ({
+            name = "${name}-dev";
+            buildInputs = [
+              pkgs.cargo-espflash
+              pkgs.cargo-generate
+              toolchain
+            ];
+          } // (import ./wifi-settings.nix));
+
+          apps.default = let
             flash = pkgs.writeShellApplication {
               name = "flash-${name}";
               runtimeInputs = [ pkgs.cargo-espflash ];
               text = ''
                 espflash --monitor ${self.packages.${system}.default}/bin/${name}
-              '';
+                              '';
             };
           in {
             type = "app";
-            program = "${pkgs.lib.getExe flash}";
+            program = "${lib.getExe flash}";
           };
-        };
-      }
+        }
     );
 }
